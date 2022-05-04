1ff7927a5d0a2849d0e562f0aa90b591ef0cb92e
TueTran <{tuemeoa13@gmail.com}>
2022-05-04 17:23:57 +0700

    update doctor details

---
+++ src/controllers/doctorController.js
@@ -0,0 +1,61 @@
+import doctorService from "../services/doctorService";
+
+let getTopDoctorHome = async(req, res) => {
+    let limit = req.query.limit;
+    if (!limit) limit = 10;
+    try {
+        let response = await doctorService.getTopDoctorHome(+limit); // use +limit because maybe limit is string and must convert limit to number
+        return res.status(200).json(response)
+    } catch (e) {
+        console.log(e);
+        return res.status(200).json({
+            errCode: -1,
+            message: 'error from server...'
+        })
+    }
+}
+
+let getAllDoctors = async(req, res) => {
+    try {
+        let doctors = await doctorService.getAllDoctors();
+        return res.status(200).json(doctors);
+    } catch (e) {
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: 'error from server...'
+        })
+    }
+}
+
+
+let postInforDoctor = async(req, res) => {
+    try {
+        let response = await doctorService.saveDetailInforDoctor(req.body);
+        return res.status(200).json(response);
+    } catch (e) {
+         return res.status(200).json({
+           errCode: -1,
+           errMessage: "error from server...",
+         });
+    }
+}
+
+let getDetailDoctorById = async(req, res) => {
+    try {
+        let infor = await doctorService.getDetailDoctorById(req.query.id);
+        return res.status(200).json(infor);
+    } catch (e) {
+        console.log(e);
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: "error from server...",
+        })
+    }
+}
+
+module.exports = {
+  getTopDoctorHome: getTopDoctorHome,
+  getAllDoctors: getAllDoctors,
+  postInforDoctor: postInforDoctor,
+  getDetailDoctorById: getDetailDoctorById,
+};
