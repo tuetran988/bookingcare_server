4c82d283ed961b519f84f33eb0dde96450198501
TueTran <{tuemeoa13@gmail.com}>
2022-05-13 15:22:38 +0700

    add schedule in specialty

--- src/services/specialtyService.js
+++ src/services/specialtyService.js
@@ -50,7 +50,54 @@ let getAllSpecialty = () => {
   });
 };
 
+let getDetailSpecialtyById = (inputId, location) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      if (!inputId || !location) {
+        resolve({
+          errCode: 1,
+          errMessage: "Missing request parameter id",
+        });
+      } else {
+        let data = {};
+        data = await db.Specialty.findOne({
+          where: { id: inputId },
+          attributes: ["descriptionHTML", "descriptionMarkdown"],
+        });
+        if (data) {
+          let doctorSpecialty=[];
+          if (location === 'ALL') {
+             doctorSpecialty = await db.Doctor_Infor.findAll({
+               where: { specialtyId: inputId },
+               attributes: ["doctorId", "provinceId"],
+             });
+          } else {
+             doctorSpecialty = await db.Doctor_Infor.findAll({
+               where: {
+                 specialtyId: inputId,
+                 provinceId: location
+               },
+               attributes: ["doctorId", "provinceId"],
+             });
+          }
+          data.doctorSpecialty = doctorSpecialty;
+        } else {
+          data = {};
+        }
+        resolve({
+          errCode: 0,
+          errMessage: "ok",
+          data: data,
+        });
+      }
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
 module.exports = {
   createSpecialty: createSpecialty,
   getAllSpecialty: getAllSpecialty,
+  getDetailSpecialtyById: getDetailSpecialtyById,
 };
